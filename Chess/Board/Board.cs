f458a4a325b9aff45d3ef337a6dd0549dfa7e67e
filipe.pereira <filipe.pereira@altus.com.br>
2023-02-15 09:37:14 -0300

    starting to implement the board

---
+++ Chess/Board/Board.cs
@@ -0,0 +1,15 @@
+﻿namespace Board
+{
+    class Board
+    {
+        private const int Lines = 8;
+        private const int Columns = 8;
+
+        private Piece[,] Pieces;
+
+        public Board()
+        {
+            Pieces = new Piece[Lines, Columns];
+        }
+    }
+}
---
+++ Chess/Program.cs
@@ -0,0 +1,12 @@
+﻿using Board;
+
+namespace Chess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Position p = new Position(3, 4);
+        }
+    }
+}
