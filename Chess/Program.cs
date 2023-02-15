c267a8f83cfbcfd120a8fcb0366c66286d20477d
filipe.pereira <filipe.pereira@altus.com.br>
2023-02-15 11:01:35 -0300

    improving the board printing method

--- Chess/Program.cs
+++ Chess/Program.cs
@@ -10,7 +10,13 @@ namespace Chess
         {
             ChessPosition pos = new ChessPosition('c', 7);
             Console.WriteLine(pos.ToPosition());
-           
+
+            Board board = new Board();
+
+            board.InsertPiece(new King(board, Color.Black), new Position(0, 0));
+            board.InsertPiece(new King(board, Color.White), new Position(7, 0));
+
+            View.PrintBoard(board);           
 
             Console.ReadLine();
 
